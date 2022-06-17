614fa65b7a539ab15d39377716dcef98be0f9654
Jesús Guerrero <jgs982@gmail.com>
2022-06-17 08:45:59 +0200

    first commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,55 @@
+import Paciente from "./Paciente"
+
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
+
+    return (
+        <div className="md:w-1/2 lg:w-3/5">
+            {
+                pacientes && pacientes.length
+                ? (
+                    <>
+                        <h2 className="font-black text-3xl text-center">
+                            Listado Pacientes
+                        </h2>
+
+                        <p className="text-xl mt-5 mb-10 text-center">
+                            Administra tus {' '}
+                            <span className="text-indigo-600 font-bold">
+                                Pacientes y Citas 
+                            </span>
+                        </p>
+                    </>
+                  )
+                : (
+                    <>
+                        <h2 className="font-black text-3xl text-center">
+                            No hay Pacientes
+                        </h2>
+
+                        <p className="text-xl mt-5 mb-10 text-center">
+                            Comienza agregando pacientes {' '}
+                            <span className="text-indigo-600 font-bold">
+                                y aparecerán en este lugar 
+                            </span>
+                        </p>
+                    </>
+                  )
+            }            
+
+            {
+                pacientes.map(paciente => {
+                    return (
+                        <Paciente
+                            key={paciente.id}
+                            paciente={paciente}
+                            setPaciente={setPaciente}
+                            eliminarPaciente={eliminarPaciente}
+                        />
+                    )
+                })
+            }          
+        </div>
+    )
+}
+
+export default ListadoPacientes
